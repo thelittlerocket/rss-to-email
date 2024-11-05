c8767ed2c182186a429386f13517cd78621c3039
thelittlerocket <75967438+thelittlerocket@users.noreply.github.com>
2024-11-05 16:44:34 +0800

    Update feeds.ts

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,6 +1,6 @@
 export const feeds = [
   // LET Offers
-  'https://rss.api.net.tw/i/?a=normal&get=f_2',
+  'https://lowendtalk.com/categories/offers/feed.rss',
 
   // LET Domains
   // 'https://www.lowendtalk.com/categories/domains.rss',
